0a1a311fd16b6303d42383b0a105297fd4746d75
zhoubozhoubo <759226488@qq.com>
2019-07-30 10:42:16 +0800

    zb

--- public/CBOadminWeb/build/config.js
+++ public/CBOadminWeb/build/config.js
@@ -1,6 +1,6 @@
 let config = {
     env: 'development',
-    baseUrl: 'http://localhost:8003/admin/'
-    // baseUrl: 'http://cbo.ariesleo.cn:8003/admin/'
+    // baseUrl: 'http://localhost:8003/admin/'
+    baseUrl: 'http://cbo.ariesleo.cn:8003/admin/'
 };
 export default config;
